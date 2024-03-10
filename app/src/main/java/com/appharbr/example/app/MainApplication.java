99e81c9f868319a5a893d5a781f2a8c25a22e840
Ido Ozdova <ido.ozdova@geoedge.com>
2024-03-10 15:28:33 +0200

    Added examples of how to use the main features of AppHarbr using AHSdkConfiguration on initialization

--- app/src/main/java/com/appharbr/example/app/MainApplication.java
+++ app/src/main/java/com/appharbr/example/app/MainApplication.java
@@ -7,9 +7,14 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.appharbr.sdk.configuration.AHSdkConfiguration;
+import com.appharbr.sdk.configuration.AHSdkDebug;
+import com.appharbr.sdk.engine.AdSdk;
 import com.appharbr.sdk.engine.AppHarbr;
 import com.appharbr.sdk.engine.InitializationFailureReason;
 import com.appharbr.sdk.engine.listeners.OnAppHarbrInitializationCompleteListener;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainApplication extends Application {
 
@@ -27,10 +32,50 @@ public class MainApplication extends Application {
         initAppHarbr();
     }
 
+    /**
+     * Publishers create AHSdkConfiguration for initializing AppHarbr.
+     * AHSdkConfiguration offers various internal features for customization.
+     * We've provided a few examples in this method.
+     * You can combine these examples into a single AHSdkConfiguration tailored to your specific requirements.
+     */
+    private AHSdkConfiguration getAppHarbrRelevantConfiguration() {
+        // Regular AppHarbr Configuration
+        AHSdkConfiguration ahSdkNormalConfiguration = new AHSdkConfiguration.Builder(API_KEY).build();
+
+        // Alternatively AppHarbr Configuration with BlockAll testing
+        AHSdkConfiguration ahSdkBlockAllTestingConfiguration = new AHSdkConfiguration.Builder(API_KEY)
+                .withDebugConfig(new AHSdkDebug(true).withBlockAll(true))
+                .build();
+
+        // Alternatively AppHarbr Configuration with Targeted Ad Networks
+        AHSdkConfiguration ahSdkTargetedAdNetworksConfiguration = new AHSdkConfiguration.Builder(API_KEY)
+                .withTargetedNetworks(new AdSdk[] {AdSdk.ADMOB, AdSdk.CHARTBOOST})
+                .build();
+
+        // Alternatively AppHarbr Configuration with Interstitial time limit
+        AHSdkConfiguration ahSdkInterstitialTimeLimitConfiguration = new AHSdkConfiguration.Builder(API_KEY)
+                .withInterstitialAdTimeLimit(30) // 30 seconds
+                .build();
+
+        // Alternatively AppHarbr Configuration with Mute Autoplay Sound
+        AHSdkConfiguration ahSdkMuteAutoPlaySoundConfiguration = new AHSdkConfiguration.Builder(API_KEY)
+                .withMuteAd(true)
+                .build();
+
+        // Alternatively AppHarbr Configuration with ignoring GAM Campaigns
+        Set houseCampaignCreatives = new HashSet<>();
+        houseCampaignCreatives.add("123456");
+        houseCampaignCreatives.add("987654");
+        AHSdkConfiguration ahSdkIgnoreGAMCampaignsConfiguration = new AHSdkConfiguration.Builder(API_KEY)
+                .withIgnoreHouseCampaignCreativeIds(houseCampaignCreatives)
+                .build();
+
+        return ahSdkNormalConfiguration;
+    }
+
     private void initAppHarbr() {
-        AHSdkConfiguration ahSdkConfiguration = new AHSdkConfiguration.Builder(API_KEY).build();
         AppHarbr.initialize(getApplicationContext(),
-                ahSdkConfiguration,
+                getAppHarbrRelevantConfiguration(),
                 new OnAppHarbrInitializationCompleteListener() {
                     @Override
                     public void onSuccess() {
--- kotlinsampleapp/src/main/java/com/appharbr/kotlin/example/app/MainApplication.kt
+++ kotlinsampleapp/src/main/java/com/appharbr/kotlin/example/app/MainApplication.kt
@@ -3,6 +3,8 @@ package com.appharbr.kotlin.example.app
 import android.app.Application
 import android.util.Log
 import com.appharbr.sdk.configuration.AHSdkConfiguration
+import com.appharbr.sdk.configuration.AHSdkDebug
+import com.appharbr.sdk.engine.AdSdk
 import com.appharbr.sdk.engine.AppHarbr
 import com.appharbr.sdk.engine.InitializationFailureReason
 import com.appharbr.sdk.engine.listeners.OnAppHarbrInitializationCompleteListener
@@ -16,11 +18,65 @@ class MainApplication : Application() {
         initAppHarbr()
     }
 
+    /**
+     * Publishers create AHSdkConfiguration for initializing AppHarbr.
+     * AHSdkConfiguration offers various internal features for customization.
+     * We've provided a few examples in this method.
+     * You can combine these examples into a single AHSdkConfiguration tailored to your specific requirements.
+     */
+    private fun getAppHarbrRelevantConfiguration(): AHSdkConfiguration? {
+        // Regular AppHarbr Configuration
+        val ahSdkNormalConfiguration = AHSdkConfiguration.Builder(API_KEY).build()
+
+        // Alternatively AppHarbr Configuration with BlockAll testing
+        val ahSdkBlockAllTestingConfiguration = AHSdkConfiguration.Builder(API_KEY)
+            .withDebugConfig(AHSdkDebug(true).withBlockAll(true))
+            .build()
+
+        // Alternatively AppHarbr Configuration with Targeted Ad Networks
+        val ahSdkTargetedAdNetworksConfiguration = AHSdkConfiguration.Builder(API_KEY)
+            .withTargetedNetworks(arrayOf(AdSdk.ADMOB, AdSdk.CHARTBOOST))
+            .build()
+
+        // Alternatively AppHarbr Configuration with Interstitial time limit
+        val ahSdkInterstitialTimeLimitConfiguration = AHSdkConfiguration.Builder(API_KEY)
+            .withInterstitialAdTimeLimit(30) // 30 seconds
+            .build()
+
+        // Alternatively AppHarbr Configuration with Mute Autoplay Sound
+        val ahSdkMuteAutoPlaySoundConfiguration = AHSdkConfiguration.Builder(API_KEY)
+            .withMuteAd(true)
+            .build()
+
+        // Alternatively AppHarbr Configuration with ignoring GAM Campaigns
+        val ahSdkIgnoreGAMCampaignsConfiguration = AHSdkConfiguration.Builder(API_KEY)
+            .withIgnoreHouseCampaignCreativeIds(hashSetOf("123456", "987654"))
+            .build()
+
+        return ahSdkNormalConfiguration
+
+    }
+
     private fun initAppHarbr() {
-        val ahSdkConfiguration: AHSdkConfiguration = AHSdkConfiguration.Builder(API_KEY).build()
+        // Regular AppHarbr Configuration
+        val ahSdkNormalConfiguration: AHSdkConfiguration = AHSdkConfiguration.Builder(API_KEY).build()
+
+        // Alternatively AppHarbr Configuration with BlockAll testing
+        val ahSdkBlockAllTestingConfiguration: AHSdkConfiguration = AHSdkConfiguration.Builder(API_KEY)
+            .withDebugConfig(AHSdkDebug(true).withBlockAll(true))
+            .build()
+
+        // Alternatively AppHarbr Configuration with Targeted Ad Networks
+
+        // Alternatively AppHarbr Configuration with Targeted Ad Networks
+        val ahSdkTargetedAdNetworksConfiguration = AHSdkConfiguration.Builder(API_KEY)
+            .withTargetedNetworks(arrayOf(AdSdk.ADMOB, AdSdk.CHARTBOOST))
+            .build()
+
+
         AppHarbr.initialize(
             applicationContext,
-            ahSdkConfiguration,
+            ahSdkNormalConfiguration,
             object : OnAppHarbrInitializationCompleteListener {
 
                 override fun onSuccess() {
